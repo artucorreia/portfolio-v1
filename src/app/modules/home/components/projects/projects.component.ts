75b1b0cd1585aa832f36d5a4029079ba58c3777b
Arthur Correia <arthurcorria0109@gmail.com>
2023-08-22 19:12:30 -0300

    update

--- src/app/modules/home/components/projects/projects.component.ts
+++ src/app/modules/home/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from '../../model/project';
 
 @Component({
   selector: 'app-projects',
@@ -6,8 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
-  
-  constructor() {
+  projectList: Project[] = [
+    {title: 'titulo projeto 1', img: '#', imgAlt: 'alt projeto 1' ,about: 'descrição do projeto 1', loading: false},
+    {title: 'titulo projeto 2', img: '#', imgAlt: 'alt projeto 2' ,about: 'descrição do projeto 2', loading: true},
+    {title: 'titulo projeto 3', img: '#', imgAlt: 'alt projeto 3' ,about: 'descrição do projeto 3', loading: false}
+  ];
 
-  }
+  constructor() { }
+
 }
--- src/app/modules/home/model/project.ts
+++ src/app/modules/home/model/project.ts
@@ -1,5 +1,7 @@
 export interface Project {
     title: string;
     img: string;
+    imgAlt: string;
     about: string;
+    loading: boolean;
 }
