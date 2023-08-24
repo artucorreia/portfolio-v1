87555e1156cfe108c61e5deaa89df9e74af5e5bb
Arthur Correia <arthurcorria0109@gmail.com>
2023-08-24 13:39:59 -0300

    finalizei o header

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
@@ -14,7 +15,8 @@ import { ContactComponent } from './components/contact/contact.component';
     HomeComponent,
     AboutComponent,
     ProjectsComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
