9d270a27c41f12d77af6dcd616e60de6af53ccc3
Arthur Correia <arthurcorria0109@gmail.com>
2023-08-29 14:08:57 -0300

    update
    separei o componente em cape component e about component, adicionei minhas redes sociais no cape component

---
+++ src/app/modules/home/components/cape/cape.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cape',
+  templateUrl: './cape.component.html',
+  styleUrls: ['./cape.component.scss']
+})
+export class CapeComponent {
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,6 +6,7 @@ import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CapeComponent } from './components/cape/cape.component';
 
 
 
@@ -16,7 +17,8 @@ import { FooterComponent } from './components/footer/footer.component';
     AboutComponent,
     ProjectsComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    CapeComponent
   ],
   imports: [
     CommonModule
