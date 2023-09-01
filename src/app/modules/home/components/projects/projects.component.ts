e2853a9fedfd2daccb1272ee5c8dd24d464dca22
Arthur Correia <arthurcorria0109@gmail.com>
2023-08-31 21:34:07 -0300

    update

--- src/app/modules/home/components/projects/projects.component.ts
+++ src/app/modules/home/components/projects/projects.component.ts
@@ -10,7 +10,7 @@ export class ProjectsComponent {
   projectList: Project[] = [
     {
       title: 'calculadora', 
-      img: '../../../../../assets/jpeg/calculadora.jpg', 
+      img: 'assets/jpeg/calculadora.jpg', 
       imgAlt: 'projeto calculadora',
       about: 'Calculadora feita usando apenas HTML, CSS e JavaScript. Possui as quatro operações básicas', 
       linkGitHub: 'https://github.com/artucorreia/calculadora',
@@ -18,7 +18,7 @@ export class ProjectsComponent {
     },
     {
       title: 'to-do list angular', 
-      img: '../../../../../assets/jpeg/todo-list-angular.jpg', 
+      img: 'assets/jpeg/todo-list-angular.jpg', 
       imgAlt: 'projeto to-do list Angular',
       about: 'Lista de tarefas, feita com o framework Angular. Possui as opções de marcar tarefa como feita, deletar tarefas e também de edita-las, também possui localStorage', 
       linkGitHub: 'https://github.com/artucorreia/app-todo-list',
@@ -26,7 +26,7 @@ export class ProjectsComponent {
     },
     {
       title: 'to-do list js', 
-      img: '../../../../../assets/jpeg/todo-list-js.jpg', 
+      img: 'assets/jpeg/todo-list-js.jpg', 
       imgAlt: 'projeto to-do list JS' ,
       about: 'Lista de tarefas fieta utilizando HTML, CSS, JavaScript e Bootstrap. O usuário pode editar suas tarefas, marcar como feita e também deletar, nesses casos as tarefas vão para suas páginas correspondentes "concluidas" ou "deletadas". Possui localStorage', 
       linkGitHub: 'https://github.com/artucorreia/lista-tarefas',
